faf7ec8b8a107157f2256fc73b024f3c26923532
Haojie Zhang <zhj630985214@gmail.com>
2024-02-26 16:24:07 +0800

    完成（七）显示得分与游戏文本、（八）构建游戏

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour {
     private Rigidbody _rb;
@@ -11,12 +12,18 @@ public class PlayerController : MonoBehaviour {
     public float speed = 0.0f;
     public float deceleration = 0.0f;
     private Keyboard _keyboard;
-    private static string PICKUP_TAG = "PickUp";
+    private static readonly string PickupTag = "PickUp";
+    private int _count; // 已经收集的物品数量
+    public TextMeshProUGUI countText;
+    public GameObject winText;
 
     // Start is called before the first frame update
     private void Start() {
         _rb = GetComponent<Rigidbody>();
         _keyboard = Keyboard.current;
+        _count = 0;
+        SetCountText();
+        winText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -37,6 +44,13 @@ public class PlayerController : MonoBehaviour {
         _movementX = movementVector.x;
         _movementY = movementVector.y;
     }
+    
+    private void SetCountText() {
+        countText.text = "Count: " + _count.ToString();
+        if (_count >= 12) {
+            winText.SetActive(true);
+        }
+    }
 
     private void OnTriggerEnter(Collider other) {
         // `OnTriggerEnter`是Unity引擎中的一个方法，它在一个游戏对象的碰撞器（Collider）首次进入另一个游戏对象的触发器（Trigger）时被调用。
@@ -44,8 +58,10 @@ public class PlayerController : MonoBehaviour {
         // 需要将`Is Trigger`属性设置为`true`的碰撞器才能触发`OnTriggerEnter`方法。
         // 如果一个碰撞器的`Is Trigger`属性被设置为`true`，那么它将不会对其他碰撞器产生物理效果，但是它可以触发其他碰撞器的`OnTriggerEnter`方法。
         var go = other.gameObject;
-        if (go.CompareTag(PICKUP_TAG)) {
+        if (go.CompareTag(PickupTag)) {
             go.SetActive(false);
+            _count++;
+            SetCountText();
         }
     }
 }
